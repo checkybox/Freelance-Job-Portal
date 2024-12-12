64bd717827d68a0c8194485287e5609f38666d8b
Vitaliy Golubenko <checkybox@proton.me>
2024-12-12 13:53:30 +0500

    Added get and set method to Freelancer class

--- src/Project/Freelancer.java
+++ src/Project/Freelancer.java
@@ -2,5 +2,46 @@ package Project;
 
 public class Freelancer
 {
+    private String name;
+    private int age;
+    private String job;
 
+    public Freelancer(String name, int age, String job)
+    {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    // get methods
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public String getJob()
+    {
+        return this.job;
+    }
+
+    // set methods
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public void setJob(String job)
+    {
+        this.job = job;
+    }
 }

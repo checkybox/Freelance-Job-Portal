468eaae67dddca2171ea8390397f72e54f5c7963
Vitaliy Golubenko <checkybox@proton.me>
2024-12-12 13:53:30 +0500

    Added get and set methods to Freelancer class

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // placeholder
+        Freelancer freelancer = new Freelancer("John", 28, "Data Scientist");
+        System.out.println(freelancer.getName());
+        System.out.println(freelancer.getAge());
+        System.out.println(freelancer.getJob());
     }
 }

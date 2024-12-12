afd43ed1aeecfa58f49d15db59791300785d0493
Vitaliy Golubenko <checkybox@proton.me>
2024-12-12 13:21:22 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Project.Job;
+import Project.Freelancer;
+import Project.JobPortal;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // placeholder
+    }
+}

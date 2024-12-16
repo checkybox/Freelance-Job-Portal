160d0ef69047f37503cac4517781e5034eca9148
Vitaliy Golubenko <checkybox@proton.me>
2024-12-16 19:32:09 +0500

    Output comparison between two class instances

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,22 @@ public class Main
         // create the second freelancer class instance
         Freelancer freelancer2 = new Freelancer("Nikola", 43, "Software Engineer");
 
+        // create several jobPortal instances
         JobPortal jobPortal1 = new JobPortal("HeadHunter", "Russia");
         JobPortal jobPortal2 = new JobPortal("Craigslist", "USA");
         JobPortal jobPortal3 = new JobPortal("Fiverr", "Multinational online marketplace for freelance services","Israel");
+
+        // check if freelancer2 is older than freelancer1
+        if(freelancer2.getAge() > freelancer1.getAge())
+        {
+            System.out.println(freelancer2.getName() + " is older than " + freelancer1.getName());
+        }
+        else
+        {
+            System.out.println(freelancer2.getName() + " is not older than " + freelancer1.getName());
+        }
+
+        // do the same using the isOlderThan method
+        freelancer1.isOlderThan(freelancer2);
     }
 }

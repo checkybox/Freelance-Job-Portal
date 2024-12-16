14e99b78e91f0b9cb35a24217fae4cc36375efd0
Vitaliy Golubenko <checkybox@proton.me>
2024-12-16 19:37:52 +0500

    Add more output data

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,22 @@ public class Main
         System.out.println(freelancer1.getName());
         System.out.println(freelancer1.getAge());
         System.out.println(freelancer1.getJob());
+        System.out.println();
 
         // create the second freelancer class instance
         Freelancer freelancer2 = new Freelancer("Nikola", 43, "Software Engineer");
 
-        // create several jobPortal instances
+        // create several jobPortal instances and output their properties
         JobPortal jobPortal1 = new JobPortal("HeadHunter", "Russia");
         JobPortal jobPortal2 = new JobPortal("Craigslist", "USA");
         JobPortal jobPortal3 = new JobPortal("Fiverr", "Multinational online marketplace for freelance services","Israel");
+        System.out.println(jobPortal1.getJobPortalName() + " : " + jobPortal1.getOrigin());
+        System.out.println(jobPortal2.getJobPortalName() + " : " + jobPortal2.getOrigin());
+        System.out.println(jobPortal3.getJobPortalName() + " : " + jobPortal3.getOrigin());
+        System.out.println();
 
         // check if freelancer2 is older than freelancer1
+        System.out.println("Checking if " + freelancer2.getName() + " is older than " + freelancer1.getName() + "...");
         if(freelancer2.getAge() > freelancer1.getAge())
         {
             System.out.println(freelancer2.getName() + " is older than " + freelancer1.getName());
@@ -30,8 +36,10 @@ public class Main
         {
             System.out.println(freelancer2.getName() + " is not older than " + freelancer1.getName());
         }
+        System.out.println();
 
         // do the same using the isOlderThan method
+        System.out.println("Checking if " + freelancer1.getName() + " is older than " + freelancer2.getName() + "...");
         freelancer1.isOlderThan(freelancer2);
     }
 }
